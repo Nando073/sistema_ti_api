a9fd617641ac05f8c4114c742c2193bd6befcfd9
fercho <fpaniagua2023@incos-santacruz.edu.bo>
2026-07-23 02:45:04 -0400

    Agregar ruta raíz y redirección /api-docs

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return '¡API Soporte TI funcionando correctamente! Visita /api-docs para ver la documentación.';
+});
+
+// Ruta para redirigir /api-docs a la documentación Swagger
+Route::get('/api-docs', function () {
+    return redirect('/docs/index.html');
 });
