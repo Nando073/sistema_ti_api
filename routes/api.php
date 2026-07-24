6085201f765d754000241599112024c7e64ec932
fercho <fpaniagua2023@incos-santacruz.edu.bo>
2026-07-24 10:18:56 -0400

    Actualizacion

--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,27 @@ Route::apiResource('proveedores', ProveedorController::class)
     ->parameters([
         'proveedores' => 'id'
     ]);
+Route::patch('proveedores/{id}/reactivar', [ProveedorController::class, 'reactivar']);
+
 // COMPRAS
-Route::apiResource('compras', CompraController::class);
+Route::apiResource('compras', CompraController::class)->parameters(['compras' => 'id']);
+Route::patch('compras/{id}/reactivar', [CompraController::class, 'reactivar']);
+Route::get('compras/proveedor/{id_proveedor}', [CompraController::class, 'getByProveedor']);
 
 // DETALLE COMPRAS
-Route::apiResource('detalle-compras', DetalleCompraController::class);
+Route::apiResource('detalle-compras', DetalleCompraController::class)->parameters(['detalle-compras' => 'id']);
+Route::get('detalle-compras/compra/{id_compra}', [DetalleCompraController::class, 'getByCompra']);
 
 // OFERTAS
-Route::apiResource('ofertas', OfertaController::class);
+Route::apiResource('ofertas', OfertaController::class)->parameters(['ofertas' => 'id']);
+Route::patch('ofertas/{id}/reactivar', [OfertaController::class, 'reactivar']);
 
 // COTIZACIONES
-Route::apiResource('cotizaciones', CotizacionController::class);
+Route::apiResource('cotizaciones', CotizacionController::class)->parameters(['cotizaciones' => 'id']);
+Route::patch('cotizaciones/{id}/reactivar', [CotizacionController::class, 'reactivar']);
+Route::get('cotizaciones/oferta/{id_oferta}', [CotizacionController::class, 'getByOferta']);
+Route::get('cotizaciones/cliente/{id_cliente}', [CotizacionController::class, 'getByCliente']);
 
 // DETALLE COTIZACIONES
-Route::apiResource('detalle-cotizaciones', DetalleCotizacionController::class);
+Route::apiResource('detalle-cotizaciones', DetalleCotizacionController::class)->parameters(['detalle-cotizaciones' => 'id']);
+Route::get('detalle-cotizaciones/cotizacion/{id_cotizacion}', [DetalleCotizacionController::class, 'getByCotizacion']);
